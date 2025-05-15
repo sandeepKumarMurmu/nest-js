10a8f8a13a0677bf64ba1aff7c30cbd6d73186d2
ALLEN-SandeepKumarMurmu <sandeep.murmu@allen.in>
2025-05-15 15:57:07 +0530

    done

--- nest-js-p1/src/user/user.controller.ts
+++ nest-js-p1/src/user/user.controller.ts
@@ -12,6 +12,7 @@ export class UserController {
 
     @Get(":id")
     getUserById(@Param('id') id: any): any {
+        
         const userService = new UserService();
         return userService.getUserById(+id);
     }
